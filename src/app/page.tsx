7191aa4a032df0e709edadf6a4bc80c0d4491f8d
Best Olumese <Amiolemen Best>
2025-07-23 00:09:35 +0100

    03: tRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default async function Home() {
-  return <div>Hello World</div>;
-}
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "Best" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
